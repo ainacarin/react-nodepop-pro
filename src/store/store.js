759848690d30105b8d3b34c190cfd9f21b902272
Aida N <nacarinobazaga@gmail.com>
2021-05-16 09:59:17 +0200

    feat: add redux devtools devDependencies

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import { authReducer } from '../reducers/authReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const reducers = combineReducers({
     auth: authReducer
 })
 
-
-export const store = createStore(reducers);
+export const store = createStore(reducers, composeWithDevTools());
