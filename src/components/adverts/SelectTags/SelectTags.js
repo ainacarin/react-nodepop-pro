9cf47c7563401fbe36da3579debe9c35206723e5
Aida N <nacarinobazaga@gmail.com>
2021-05-21 09:17:09 +0200

    style: clean commented code

--- src/components/adverts/SelectTags/SelectTags.js
+++ src/components/adverts/SelectTags/SelectTags.js
@@ -2,20 +2,17 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { tagsLoadAction } from "../../../store/actions/tags";
 
-// import { getTags } from '../../../api/adverts';
 import { getTags } from "../../../store/selectors/tags";
 import { getUi } from "../../../store/selectors/ui";
 import { CheckboxGroup } from "../../shared";
 
 function SelectTags(props) {
-  // const [tags, setTags] = React.useState([]);
 
   const tags = useSelector(getTags);
   const { loading, error } = useSelector(getUi);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    // getTags().then(setTags);
     dispatch(tagsLoadAction());
   }, []);
 
