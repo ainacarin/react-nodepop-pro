7942df661b8089efe84b08f9f802ae274bf81314
Aida N <nacarinobazaga@gmail.com>
2021-05-24 16:26:15 +0200

    style: clean code selectors adverts

--- src/store/selectors/adverts.js
+++ src/store/selectors/adverts.js
@@ -3,10 +3,10 @@ const mapAdvert = ({ photo, ...advert }) => ({
   photo: photo ? `${process.env.REACT_APP_API_BASE_URL}${photo}` : photo,
 });
 export const getAdverts = (state) => state.adverts.data;
-  export const getAdvertsLoaded = (state) => state.adverts.loaded;
-// export const getAdvertState = (id) => (state) => mapAdvert(state.adverts.data.find(advert => advert.id === id));
-export const getAdvertState = (id) => (state) => state.adverts.data.find(advert => advert.id === id);
+export const getAdvertsLoaded = (state) => state.adverts.loaded;
+export const getAdvertState = (id) => (state) =>
+  state.adverts.data.find((advert) => advert.id === id);
 
 export const getAdvertDetail = (state, id) => {
-  return state.adverts?.data.find(advert => advert.id === id);
+  return state.adverts?.data.find((advert) => advert.id === id);
 };
