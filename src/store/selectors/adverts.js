3bd9fa0583254e36c9cda1b372f4b74eb649b7f9
Aida N <nacarinobazaga@gmail.com>
2021-05-27 16:06:45 +0200

    style: clean code adverts selector

--- src/store/selectors/adverts.js
+++ src/store/selectors/adverts.js
@@ -1,7 +1,4 @@
-const mapAdvert = ({ photo, ...advert }) => ({
-  ...advert,
-  photo: photo ? `${process.env.REACT_APP_API_BASE_URL}${photo}` : photo,
-});
+
 export const getAdverts = (state) => state.adverts.data;
 export const getAdvertsLoaded = (state) => state.adverts.loaded;
 export const getAdvertState = (id) => (state) =>
